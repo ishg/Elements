78b2a62acb7f7b6bbe0cba094ca8b996312d1dd2
Ishmeet Grewal <ishmeetsinghgrewal@gmail.com>
2014-11-25 22:49:03 -0500

    Added diagonal elimination and power-ups

--- app/src/main/java/com/ishmeetgrewal/elements/GameActivity.java
+++ app/src/main/java/com/ishmeetgrewal/elements/GameActivity.java
@@ -3,6 +3,7 @@ package com.ishmeetgrewal.elements;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,7 +13,7 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.widget.Button;
 
 public class GameActivity extends Activity {
 
@@ -21,7 +22,7 @@ public class GameActivity extends Activity {
     int nextColor;
     ImageView nextItemView;
     boolean newGame = true;
-    int[] hasItemArray = new int[49];
+    int[] tileArray = new int[49];
     int red = R.drawable.icon_fire;
     int green = R.drawable.icon_grass;
     int blue = R.drawable.icon_water;
@@ -52,9 +53,10 @@ public class GameActivity extends Activity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 ImageView imageView = (ImageView) view;
+                /*
                 if(newGame){
                     newGame = false;
-                    hasItemArray[position]= nextItem;
+                    tileArray[position]= nextItem;
                     imageView.setImageResource(nextItem);
                     imageView.setBackgroundColor(nextColor);
                     checkNeighbors(position,nextItem);
@@ -63,8 +65,9 @@ public class GameActivity extends Activity {
                     nextItemView.setBackgroundColor(nextColor);
                     updateScore(1);
                 }
-                else if(hasItemArray[position]==0 && checkAdjacent(position)){
-                    hasItemArray[position]= nextItem;
+                else if(tileArray[position]==0 && checkAdjacent(position)){*/
+                if(tileArray[position]==0){
+                    tileArray[position]= nextItem;
                     imageView.setImageResource(nextItem);
                     imageView.setBackgroundColor(nextColor);
                     checkNeighbors(position,nextItem);
@@ -75,6 +78,29 @@ public class GameActivity extends Activity {
                 }else{
                     Toast.makeText(getApplicationContext(), "Not possible!", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        Button button_fire_power = (Button) findViewById(R.id.button_fire_power);
+        Button button_water_power = (Button) findViewById(R.id.button_water_power);
+        Button button_grass_power = (Button) findViewById(R.id.button_grass_power);
+
+        button_fire_power.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                powerUp(red);
+            }
+        });
+        button_water_power.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                powerUp(blue);
+            }
+        });
+        button_grass_power.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                powerUp(green);
             }
         });
 
@@ -119,16 +145,16 @@ public class GameActivity extends Activity {
             bottom = -1;
         }
 
-        if(top>=0 && hasItemArray[top]!=0) {
+        if(top>=0 && tileArray[top]!=0) {
             result = true;
         }
-        else if(left>=0 && hasItemArray[left]!=0){
+        else if(left>=0 && tileArray[left]!=0){
             result = true;
         }
-        else if(right>=0 && hasItemArray[right]!=0){
+        else if(right>=0 && tileArray[right]!=0){
             result = true;
         }
-        else if(bottom>=0 && hasItemArray[bottom]!=0){
+        else if(bottom>=0 && tileArray[bottom]!=0){
             result = true;
         }
         return result;
@@ -136,120 +162,123 @@ public class GameActivity extends Activity {
 
     public void checkNeighbors(int pos, int currentItem){
         //Determine Neighbors
-        int top,left,bottom,right;
+        int top,left,bottom,right,topLeft,topRight,botRight,botLeft;
         top = pos - 7;
         if(top<0){
-            top = -1;
+            top = -2;
         }
         left = pos - 1;
         if(((left+1)%7==0)||(left<0)){
-            left = -1;
+            left = -2;
         }
         right = pos + 1;
         if((right%7==0)||(right>48)){
-            right = -1;
+            right = -2;
         }
         bottom = pos + 7;
         if (bottom>48){
-            bottom = -1;
+            bottom = -2;
         }
+        topLeft = top - 1;
+        topRight = top + 1;
+        botLeft = bottom - 1;
+        botRight = bottom + 1;
 
-        GridView gridView = (GridView) findViewById(R.id.grid_view);
+
         //Analysis
         if(currentItem == red){
-            if(top>=0 && hasItemArray[top]==green){
-                hasItemArray[top]=0;
-                ImageView iv = (ImageView) gridView.getChildAt(top);
-                iv.setImageResource(blank);
-                iv.setBackgroundColor(Color.rgb(238,238,238));
-                updateScore(3);
-            }
-            if(left>=0 && hasItemArray[left]==green){
-                hasItemArray[left]=0;
-                ImageView iv = (ImageView) gridView.getChildAt(left);
-                iv.setImageResource(blank);
-                iv.setBackgroundColor(Color.rgb(238,238,238));
-                updateScore(3);
-            }
-            if(right>=0 && hasItemArray[right]==green){
-                hasItemArray[right]=0;
-                ImageView iv = (ImageView) gridView.getChildAt(right);
-                iv.setImageResource(blank);
-                iv.setBackgroundColor(Color.rgb(238,238,238));
-                updateScore(3);
-            }
-            if(bottom>=0 && hasItemArray[bottom]==green){
-                hasItemArray[bottom]=0;
-                ImageView iv = (ImageView) gridView.getChildAt(bottom);
-                iv.setImageResource(blank);
-                iv.setBackgroundColor(Color.rgb(238,238,238));
-                updateScore(3);
-            }
+            removeItems(green,top,bottom,left,right,topLeft,topRight,botLeft,botRight);
         }else if (currentItem == blue){
-            if(top>=0 && hasItemArray[top]==red){
-                hasItemArray[top]=0;
-                ImageView iv = (ImageView) gridView.getChildAt(top);
-                iv.setImageResource(blank);
-                iv.setBackgroundColor(Color.rgb(238,238,238));
-                updateScore(3);
-            }
-            if(left>=0 && hasItemArray[left]==red){
-                hasItemArray[left]=0;
-                ImageView iv = (ImageView) gridView.getChildAt(left);
-                iv.setImageResource(blank);
-                iv.setBackgroundColor(Color.rgb(238,238,238));
-                updateScore(3);
-            }
-            if(right>=0 && hasItemArray[right]==red){
-                hasItemArray[right]=0;
-                ImageView iv = (ImageView) gridView.getChildAt(right);
-                iv.setImageResource(blank);
-                iv.setBackgroundColor(Color.rgb(238,238,238));
-                updateScore(3);
-            }
-            if(bottom>=0 && hasItemArray[bottom]==red){
-                hasItemArray[bottom]=0;
-                ImageView iv = (ImageView) gridView.getChildAt(bottom);
-                iv.setImageResource(blank);
-                iv.setBackgroundColor(Color.rgb(238,238,238));
-                updateScore(3);
-            }
+            removeItems(red,top,bottom,left,right,topLeft,topRight,botLeft,botRight);
         }else{
-            if(top>=0 && hasItemArray[top]==blue){
-                hasItemArray[top]=0;
-                ImageView iv = (ImageView) gridView.getChildAt(top);
-                iv.setImageResource(blank);
-                iv.setBackgroundColor(Color.rgb(238,238,238));
-                updateScore(3);
-            }
-            if(left>=0 && hasItemArray[left]==blue){
-                hasItemArray[left]=0;
-                ImageView iv = (ImageView) gridView.getChildAt(left);
-                iv.setImageResource(blank);
-                iv.setBackgroundColor(Color.rgb(238,238,238));
-                updateScore(3);
-            }
-            if(right>=0 && hasItemArray[right]==blue){
-                hasItemArray[right]=0;
-                ImageView iv = (ImageView) gridView.getChildAt(right);
-                iv.setImageResource(blank);
-                iv.setBackgroundColor(Color.rgb(238,238,238));
-                updateScore(3);
-            }
-            if(bottom>=0 && hasItemArray[bottom]==blue){
-                hasItemArray[bottom]=0;
-                ImageView iv = (ImageView) gridView.getChildAt(bottom);
-                iv.setImageResource(blank);
-                iv.setBackgroundColor(Color.rgb(238,238,238));
-                updateScore(3);
-            }
+            removeItems(blue,top,bottom,left,right,topLeft,topRight,botLeft,botRight);
         }
+    }
+
+    public void removeItems(int color, int top, int bottom, int left, int right, int topLeft,int topRight,int bottomLeft,int bottomRight){
+
+        GridView gridView = (GridView) findViewById(R.id.grid_view);
+        ImageView iv;
+
+        if(top>=0 && tileArray[top]==color){
+            tileArray[top]=0;
+            iv = (ImageView) gridView.getChildAt(top);
+            iv.setImageResource(blank);
+            iv.setBackgroundColor(Color.rgb(238,238,238));
+            updateScore(3);
+        }
+        if(left>=0 && tileArray[left]==color){
+            tileArray[left]=0;
+            iv = (ImageView) gridView.getChildAt(left);
+            iv.setImageResource(blank);
+            iv.setBackgroundColor(Color.rgb(238,238,238));
+            updateScore(3);
+        }
+        if(right>=0 && tileArray[right]==color){
+            tileArray[right]=0;
+            iv = (ImageView) gridView.getChildAt(right);
+            iv.setImageResource(blank);
+            iv.setBackgroundColor(Color.rgb(238,238,238));
+            updateScore(3);
+        }
+        if(bottom>=0 && tileArray[bottom]==color){
+            tileArray[bottom]=0;
+            iv = (ImageView) gridView.getChildAt(bottom);
+            iv.setImageResource(blank);
+            iv.setBackgroundColor(Color.rgb(238,238,238));
+            updateScore(3);
+        }
+        if(topLeft>=0 && tileArray[topLeft]==color){
+            tileArray[topLeft]=0;
+            iv = (ImageView) gridView.getChildAt(topLeft);
+            iv.setImageResource(blank);
+            iv.setBackgroundColor(Color.rgb(238,238,238));
+            updateScore(3);
+        }
+        if(topRight>=0 && tileArray[topRight]==color){
+            tileArray[topRight]=0;
+            iv = (ImageView) gridView.getChildAt(topRight);
+            iv.setImageResource(blank);
+            iv.setBackgroundColor(Color.rgb(238,238,238));
+            updateScore(3);
+        }
+        if(bottomLeft>=0 && tileArray[bottomLeft]==color){
+            tileArray[bottomLeft]=0;
+            iv = (ImageView) gridView.getChildAt(bottomLeft);
+            iv.setImageResource(blank);
+            iv.setBackgroundColor(Color.rgb(238,238,238));
+            updateScore(3);
+        }
+        if(bottomRight>=0 && tileArray[bottomRight]==color){
+            tileArray[bottomRight]=0;
+            iv = (ImageView) gridView.getChildAt(bottomRight);
+            iv.setImageResource(blank);
+            iv.setBackgroundColor(Color.rgb(238,238,238));
+            updateScore(3);
+        }
+
     }
 
     public void updateScore(int pos){
         score+=pos;
         TextView scoreView = (TextView) findViewById(R.id.score_count);
         scoreView.setText(Integer.toString(score));
+    }
+
+    public void powerUp(int color){
+        ImageView iv;
+        GridView gridView = (GridView) findViewById(R.id.grid_view);
+        int i = 0;
+        while(i<49){
+            if(tileArray[i]==color){
+                tileArray[i]=0;
+                iv = (ImageView) gridView.getChildAt(i);
+                iv.setImageResource(blank);
+                iv.setBackgroundColor(Color.rgb(238,238,238));
+                updateScore(3);
+            }
+            i++;
+        }
     }
 
     @Override
